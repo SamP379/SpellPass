54fcbb9f60b9df782e0cad38de96726167675aa5
Sam Patel <sampatel379@gmail.com>
2025-04-14 11:13:50 +0100

    Added get random words function to word_utils.py

--- word_utils.py
+++ word_utils.py
@@ -1,6 +1,15 @@
 import requests
+import wonderwords 
 
 DICTIONARY_ENDPOINT = "https://api.dictionaryapi.dev/api/v2/entries/en/"
+
+
+def get_random_words():
+    """Returns a list of random words."""
+    wonderword = wonderwords.RandomWord() 
+    random_words = wonderword.random_words(amount = 10, word_min_length = 3, word_max_length = 14)
+    return random_words
+
 
 def get_definition(word : str):
     """Returns the definition of a given word or None if not found"""
