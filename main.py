f04df3bb2f3aefec956bffee4f3291bcdecb48e2
Sam Patel <sampatel379@gmail.com>
2025-04-14 11:21:47 +0100

    Update

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 import os
 import time
 import word_utils
-import wonderwords 
-
-
-def main():
-
-    wonderword = wonderwords.RandomWord() 
-    random_words = wonderword.random_words(amount = 10, word_min_length = 3, word_max_length = 14)
-
-    os.system("cls")
-    for word in random_words:
-        print(word)
-        time.sleep(5)
-        os.system("cls")
-        user_guess = input("\nEnter word: ").strip().lower()
-        user_definition = input("Enter definition: ")
-        print(f"\nWord: {word}")
-        word_definition = word_utils.get_definition(word)
-        if word_definition is not None:
-            print(f"Word definition: {word_definition}")
-        time.sleep(5)
-        os.system("cls")
 
 
 
-        
+class WonderWord:
 
+    WORDS_FILE_PATH = "words.csv"
+
+    def __init__(self):
+        self.words = self.get_words()
     
-main()
+
+    def get_words(self):
+        if os.path.exists(WonderWord.WORDS_FILE_PATH):
+            pass
+        else:
+            words = word_utils.get_random_words(amount = 10)
+            return words
+    
+
+    def clear_screen(self):
+        os.system("cls")
+        
+    
+    def main_loop(self):
+
+        self.clear_screen()
+        for word in self.words:
+            print(word)
+            time.sleep(4)
+            self.clear_screen()
+            user_guess = input("\nEnter word: ").strip().lower()
+            user_definition = input("Enter definition: ")
+            print(f"\nWord: {word}")
+            word_definition = word_utils.get_definition(word)
+            if word_definition is not None:
+                print(f"Word definition: {word_definition}")
+            input("")
+            self.clear_screen()
