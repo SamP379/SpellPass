01af40b5f7aecce7c423fea9f21ca0953f1a8442
Sam Patel <sampatel379@gmail.com>
2025-04-14 11:35:10 +0100

    Added pronounce word function to word_utils.py

--- word_utils.py
+++ word_utils.py
@@ -1,13 +1,21 @@
+import pyttsx3
 import requests
 import wonderwords 
 
 DICTIONARY_ENDPOINT = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 
 
-def get_random_words():
+def pronounce_word(word : str):
+    """Uses pyttsx3 text-to-speech to pronounce a given word"""
+    engine = pyttsx3.init()
+    engine.say(word)
+    engine.runAndWait()
+
+
+def get_random_words(amount : int) -> list:
     """Returns a list of random words."""
     wonderword = wonderwords.RandomWord() 
-    random_words = wonderword.random_words(amount = 10, word_min_length = 3, word_max_length = 14)
+    random_words = wonderword.random_words(amount, word_min_length = 3, word_max_length = 14)
     return random_words
 
 
