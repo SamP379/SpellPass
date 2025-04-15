35f66b59173b52594c1acf785d21c4180fa7a1e8
Sam Patel <sampatel379@gmail.com>
2025-04-15 13:58:59 +0100

    Added sanitize_definition() function to word_utils.py

--- word_utils.py
+++ word_utils.py
@@ -1,9 +1,13 @@
 import pyttsx3
 import requests
+import ai_utils
 import wonderwords 
+
 
 DICTIONARY_ENDPOINT = "https://api.dictionaryapi.dev/api/v2/entries/en/"
 WONDERWORD = wonderwords.RandomWord() 
+MAX_DEFINITION_LENGTH = 13
+SHORTEN_DEFINTION_PROMPT = "Shorten this definition so that it's less than 13 words: "
 
 
 def pronounce(word : str):
@@ -27,13 +31,23 @@ def get_random_words(amount : int) -> dict:
     return words
 
 
+def sanitize_definition(word_definition : str) -> str:
+    if len(word_definition.split()) > MAX_DEFINITION_LENGTH:
+        prompt = SHORTEN_DEFINTION_PROMPT + word_definition
+        short_definition = ai_utils.get_llama_response(prompt) 
+    if short_definition is not None:
+        return short_definition
+    else:
+        return word_definition
+
+
 def get_definition(word : str) -> str|None:
     """Returns the definition of a given word or None if not found"""
     try:
         endpoint = DICTIONARY_ENDPOINT + word
         response = requests.get(url = endpoint)
-        content = response.json()
-        definition = content[0]["meanings"][0]["definitions"][0]["definition"]
+        definition = response.json()[0]["meanings"][0]["definitions"][0]["definition"]
+        #definition = sanitize_definition(definition)
         return definition
     except Exception as error:
         print(f"An error occured: {error}")
