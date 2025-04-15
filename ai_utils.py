9c5cc107650748cabfe466cc31384cbcf743a0fb
Sam Patel <sampatel379@gmail.com>
2025-04-15 13:59:29 +0100

    Created ai_utils.py

---
+++ ai_utils.py
@@ -0,0 +1,13 @@
+
+from langchain_ollama import OllamaLLM
+
+
+def get_llama_response(prompt : str) -> str:
+    """Generates a response from the llama3.2 LLM using a given prompt."""
+    try:
+        model = OllamaLLM(model = "llama3.2")
+        response = model.invoke(input = prompt)
+        return response
+    except Exception as error:
+        print(f"An error occured: {error}")
+        return None
