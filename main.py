dc05891e3212a185246063621578d90f5d030982
Sam Patel <sampatel379@gmail.com>
2025-04-16 11:57:03 +0100

    Created main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from wonderword import WonderWord
+
+PRACTISE_WORDS_AMOUNT = 5
+
+def main():
+    wonderword = WonderWord(PRACTISE_WORDS_AMOUNT)
+    wonderword.main_loop()
+
+if __name__ == "__main__":
+    main()
